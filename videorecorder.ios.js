501cfae0439c937d1841b47d1d7f5625587bcec9
Ricardo Bocchi <ricardo@mobilemind.com.br>
2017-06-09 14:34:53 -0300

    ios bugfix

--- videorecorder.ios.js
+++ videorecorder.ios.js
@@ -1,17 +1,24 @@
 "use strict";
-var __extends = (this && this.__extends) || function (d, b) {
-    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
-    function __() { this.constructor = d; }
-    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-};
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+exports.__esModule = true;
 var listener;
-var VideoRecorder = (function () {
+var VideoRecorder = (function(){
     function VideoRecorder() {
     }
-    VideoRecorder.prototype.record = function (options) {
-        return new Promise(function (resolve, reject) {
+    VideoRecorder.prototype.record =  function(options) {
+
+        return new Promise( function(resolve, reject) {
             listener = null;
-            var videoEditorController = new UIVideoEditorController();
+            var pickerController = new UIImagePickerController();
             var saveToGallery = true;
             var quality = 0;
             var duration = 0.0;
@@ -20,73 +27,119 @@ var VideoRecorder = (function () {
                 quality = options.hd ? 1 : 0;
                 duration = options.duration || 0.0;
             }
+            console.log("step 1")
+
             if (saveToGallery) {
-                listener = UIVideoEditorControllerDelegateImpl["new"]().initWithCallbackAndOptions(resolve, { duration: duration, quality: quality, saveToGallery: saveToGallery });
+                listener = UIImagePickerControllerDelegateImpl["new"]().initWithCallbackAndOptions(resolve, reject, { duration: duration, quality: quality, saveToGallery: saveToGallery });
             }
             else {
-                listener = UIVideoEditorControllerDelegateImpl["new"]().initWithCallback(resolve);
+                listener = UIImagePickerControllerDelegateImpl["new"]().initWithCallback(resolve, reject);
             }
-            videoEditorController.delegate = listener;
+
+            console.log("step 2")
+
+            pickerController.delegate = listener;
             var sourceType = UIImagePickerControllerSourceType.UIImagePickerControllerSourceTypeCamera;
-            var mediaTypes = UIImagePickerController.availableMediaTypesForSourceType(sourceType);
+            var mediaTypes = [kUTTypeMovie]
+
+            console.log("step 3")
+
             if (mediaTypes) {
-                videoEditorController.mediaTypes = mediaTypes;
-                videoEditorController.sourceType = sourceType;
+                pickerController.mediaTypes = mediaTypes;
+                pickerController.sourceType = sourceType;
             }
-            videoEditorController.modalPresentationStyle = UIModalPresentationStyle.UIModalPresentationCurrentContext;
+
+            console.log("step 4")
+
+            if(duration > 0){
+                pickerController.videoMaximumDuration = duration
+                pickerController.allowsEditing = false
+            }
+
+            console.log("step 5")
+
+            pickerController.modalPresentationStyle = UIModalPresentationStyle.UIModalPresentationCurrentContext;
             var frame = require("ui/frame");
             var topMostFrame = frame.topmost();
+
             if (topMostFrame) {
                 var viewController = topMostFrame.currentPage && topMostFrame.currentPage.ios;
                 if (viewController) {
-                    viewController.presentViewControllerAnimatedCompletion(videoEditorController, true, null);
+                    viewController.presentViewControllerAnimatedCompletion(pickerController, true, null);
                 }
             }
         });
     };
     return VideoRecorder;
 }());
+
 exports.VideoRecorder = VideoRecorder;
-var UIVideoEditorControllerDelegateImpl = (function (_super) {
-    __extends(UIVideoEditorControllerDelegateImpl, _super);
-    function UIVideoEditorControllerDelegateImpl() {
-        return _super.apply(this, arguments) || this;
+
+var UIImagePickerControllerDelegateImpl = ( function(_super) {
+    
+    __extends(UIImagePickerControllerDelegateImpl, _super);
+
+    function UIImagePickerControllerDelegateImpl() {
+        return _super !== null && _super.apply(this, arguments) || this;
     }
-    UIVideoEditorControllerDelegateImpl["new"] = function () {
-        return _super["new"].call(this);
+        
+    UIImagePickerControllerDelegateImpl.prototype.initWithCallback = function(callback, error) {
+        var that = UIImagePickerControllerDelegateImpl.new();
+        that._callback = callback;
+        that._error = error;
+        return that;
     };
-    UIVideoEditorControllerDelegateImpl.prototype.initWithCallback = function (callback) {
-        this._callback = callback;
-        return this;
-    };
-    UIVideoEditorControllerDelegateImpl.prototype.initWithCallbackAndOptions = function (callback, options) {
-        this._callback = callback;
+    
+    UIImagePickerControllerDelegateImpl.prototype.initWithCallbackAndOptions = function(callback, error, options) {
+        var that = UIImagePickerControllerDelegateImpl.new();
+        that._callback = callback;
+        that._error = error;
         if (options) {
-            this.videoQuality = options.quality;
-            this.videoMaximumDuration = options.duration;
-            this._saveToGallery = options.saveToGallery;
+            that.videoQuality = options.quality;
+            that.videoMaximumDuration = options.duration;
+            that._saveToGallery = options.saveToGallery;
         }
-        return this;
+        return that;
     };
-    UIVideoEditorControllerDelegateImpl.prototype.didSaveEditedVideoToPath = function (editedVideoPath) {
-        if (editedVideoPath) {
-            var moviePath = editedVideoPath;
+    
+    UIImagePickerControllerDelegateImpl.prototype.imagePickerControllerDidFinishPickingMediaWithInfo = function(picker, info) {
+
+        var infoDict = NSMutableDictionary.new()
+        var mediaType = info.objectForKey(UIImagePickerControllerMediaType)
+
+        if (mediaType == "public.image" ){
+
+            if(this._error){
+                this._error("video must be selected")
+            }
+
+        }else if (mediaType = "public.movie"){
+
+            var moviePath = info.objectForKey(UIImagePickerControllerMediaURL)
+            var path = moviePath.absoluteString 
+            
             if (this._callback) {
                 if (this._saveToGallery) {
-                    if (UIVideoAtPathIsCompatibleWithSavedPhotosAlbum(moviePath)) {
-                        UISaveVideoAtPathToSavedPhotosAlbum(moviePath, null, null, null);
+                    if (UIVideoAtPathIsCompatibleWithSavedPhotosAlbum(path)) {
+                        UISaveVideoAtPathToSavedPhotosAlbum(path, null, null, null);
                     }
                 }
-                this._callback({ filePath: editedVideoPath });
+                this._callback({ file: path });
             }
-        }
+
+        }        
+        
+        picker.presentingViewController.dismissViewControllerAnimatedCompletion(true, null);
+        listener = null;    
+    }
+
+    UIImagePickerControllerDelegateImpl.prototype.imagePickerControllerDidCancel = function(picker) {
         picker.presentingViewController.dismissViewControllerAnimatedCompletion(true, null);
         listener = null;
-    };
-    UIVideoEditorControllerDelegateImpl.prototype.videoEditorControllerDidCancel = function (picker) {
-        picker.presentingViewController.dismissViewControllerAnimatedCompletion(true, null);
-        listener = null;
-    };
-    return UIVideoEditorControllerDelegateImpl;
+    }
+    
+    return UIImagePickerControllerDelegateImpl;
+
 }(NSObject));
-UIVideoEditorControllerDelegateImpl.ObjCProtocols = [UIVideoEditorControllerDelegate];
+
+UIImagePickerControllerDelegateImpl.ObjCProtocols = [UIImagePickerControllerDelegate];

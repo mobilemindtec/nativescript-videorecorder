88e21cad107c766d89cf11aab2e0022198079c1e
Ricardo Bocchi <ricardo@mobilemind.com.br>
2017-07-25 20:39:23 -0300

    NativeScript 3 upgrade android

--- videorecorder.android.js
+++ videorecorder.android.js
@@ -1,5 +1,6 @@
 "use strict";
 var permissions = require("nativescript-permissions");
+var platform = require("platform")
 var app = require("application");
 var RESULT_CANCELED = 0;
 var RESULT_OK = -1;
@@ -22,50 +23,83 @@ var VideoRecorder = (function () {
             options.hd = options.hd ? 1 : 0;
             options.saveToGallery = options.saveToGallery || false;
             options.duration = options.duration || 0;
-            options.explanation = options.explanation = "";
+            options.explanation = options.explanation || "";
             var startRecording = function () {
-                var intent = new android.content.Intent(android.provider.MediaStore.ACTION_VIDEO_CAPTURE);
-                intent.putExtra(android.provider.MediaStore.EXTRA_VIDEO_QUALITY, options.hd);
-                if (options.size > 0) {
-                    intent.putExtra(android.provider.MediaStore.EXTRA_SIZE_LIMIT, options.size * 1024 * 1024);
-                }
-                if (!options.saveToGallery) {
-                    file = new java.io.File(app.android.context.getFilesDir(), "videoCapture_" + +new Date() + ".mp4");
-                    intent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, android.net.Uri.fromFile(file));
-                }
-                else {
-                    file = new java.io.File(android.os.Environment.getExternalStoragePublicDirectory(android.os.Environment.DIRECTORY_MOVIES).getAbsolutePath() + "/" + "videoCapture_" + +new Date() + ".mp4");
-                    intent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, android.net.Uri.fromFile(file));
-                }
-                if (options.duration > 0) {
-                    intent.putExtra(android.provider.MediaStore.EXTRA_DURATION_LIMIT, options.duration);
-                }
-                if (intent.resolveActivity(app.android.currentContext.getPackageManager()) != null) {
-                    app.android.currentContext.startActivityForResult(intent, REQUEST_VIDEO_CAPTURE);
-                    app.android.on(app.AndroidApplication.activityResultEvent, function (args) {
-                        if (args.requestCode === REQUEST_VIDEO_CAPTURE && args.resultCode === RESULT_OK) {
-                            if (options.saveToGallery) {
-                                resolve({ file: file.toString() });
+
+                try{
+                    var intent = new android.content.Intent(android.provider.MediaStore.ACTION_VIDEO_CAPTURE);
+                    var fileName = 'videoCapture_'+ new Date().getTime() + '.mp4';
+                    
+                    intent.putExtra(android.provider.MediaStore.EXTRA_VIDEO_QUALITY, options.hd);
+                    
+                    if (options.size > 0) {
+                        intent.putExtra(android.provider.MediaStore.EXTRA_SIZE_LIMIT, options.size * 1024 * 1024);
+                    }
+                    
+                    if (!options.saveToGallery) {
+                        var path = android.os.Environment.getExternalStoragePublicDirectory(android.os.Environment.DIRECTORY_DCIM).getAbsolutePath() + "/Camera/" + fileName;
+                        file = new java.io.File(path);
+                        intent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, android.net.Uri.fromFile(file));
+                        
+                    }else {                                    
+                        var sdkVersionInt = parseInt(platform.device.sdkVersion);
+                        if (sdkVersionInt > 21) {
+                            
+                            var path = android.os.Environment.getExternalStoragePublicDirectory(android.os.Environment.DIRECTORY_DCIM).getAbsolutePath() + "/Camera/" + fileName;
+                            file = new java.io.File(path);
+                            var tempPictureUri = android.support.v4.content.FileProvider.getUriForFile(app.android.currentContext, app.android.nativeApp.getPackageName() + ".provider", file);
+                            
+                            intent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, tempPictureUri)
+                        } else {
+                            var path = android.os.Environment.getExternalStoragePublicDirectory(android.os.Environment.DIRECTORY_DCIM).getAbsolutePath() + "/Camera/" + fileName;
+                            file = new java.io.File(path);
+                            intent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, android.net.Uri.fromFile(file))
+                        }
+                    }
+                    if (options.duration > 0) {
+                        intent.putExtra(android.provider.MediaStore.EXTRA_DURATION_LIMIT, options.duration);
+                    }
+                    if (intent.resolveActivity(app.android.currentContext.getPackageManager()) != null) {
+                                                
+
+                        app.android.on("activityResult", function(eventData) {
+                                
+                            if (eventData.requestCode === REQUEST_VIDEO_CAPTURE && eventData.resultCode === RESULT_OK) {
+                                if (options.saveToGallery) {
+                                    resolve({ file: file.toString() });
+                                }
+                                else {
+                                    resolve({ file: file.toString() });
+                                }
+                            }
+                            else if (eventData.resultCode === RESULT_CANCELED) {
+                                reject({ event: 'cancelled' });
                             }
                             else {
-                                resolve({ file: file.toString() });
+                                reject({ event: 'failed' });
                             }
-                        }
-                        else if (args.resultCode === RESULT_CANCELED) {
-                            reject({ event: 'cancelled' });
-                        }
-                        else {
-                            reject({ event: 'failed' });
-                        }
-                    });
-                }
-                else {
-                    reject({ event: 'failed' });
+
+                        })
+
+                        app.android.currentContext.startActivityForResult(intent, REQUEST_VIDEO_CAPTURE);                        
+                        
+                    }
+                    else {
+                        reject({ event: 'failed' });
+                    }
+                }catch(error){
+                    console.log('error: ' + error)
                 }
             };
+
+            var permissionList = [
+                android.Manifest.permission.CAMERA,
+                android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+            ]
             if (currentapiVersion >= MARSHMALLOW) {
+                
                 if (options.explanation.length > 0) {
-                    permissions.requestPermission(android.Manifest.permission.CAMERA, options.explanation)
+                    permissions.requestPermission(permissionList, options.explanation)
                         .then(function () {
                         startRecording();
                     })["catch"](function () {
@@ -73,7 +107,7 @@ var VideoRecorder = (function () {
                     });
                 }
                 else {
-                    permissions.requestPermission(android.Manifest.permission.CAMERA)
+                    permissions.requestPermission(permissionList)
                         .then(function () {
                         startRecording();
                     })["catch"](function () {
